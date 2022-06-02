e6a27d8dcb5f7add38c07dbd5b7fa49fbf00fcc8
yashwanthkumar <yashwanthkumar1143@gmail.com>
2022-06-02 21:19:29 +0530

    done

---
+++ src/routes/logger/logger.js
@@ -0,0 +1,8 @@
+const Name = "yash";
+const wellcome = () => {
+    console.log(`Welcome to my application. I am ${Name} and a part of FunctionUp Radon cohort.`)
+}
+
+//  functions Exports from here
+module.exports.wellcome = wellcome; 
+
