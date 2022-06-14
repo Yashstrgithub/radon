e467f3a3444ce89146b0fb15a779663c3af56882
yashwanthkumar <yashwanthkumar1143@gmail.com>
2022-06-14 10:08:31 +0530

    done

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://functionup-cohort:Cy1gBvGK84qPiPyJ@cluster0.sngd6.mongodb.net/yash1?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
---
+++ src/middlewares/commonMiddleware.js
@@ -0,0 +1,33 @@
+const ProductModel = require("../models/productModel")
+const UserModel = require("../models/userModel")
+const isFree = function (req, res, next) {
+    let freeUser = req.headers.isfreeappuser
+    if (freeUser) {
+        next()
+    } else {
+        res.send({ msg: "The request is missing a mandatory header!" })
+    }
+}
+
+
+const isOrderIDvalid = async function (req, res, next) {
+    let order = req.body
+    let userIDOrder = order.userId;
+    let productIDOrder = order.productId;
+
+    let userID = await UserModel.findById(userIDOrder);
+    let productId = await ProductModel.findById(productIDOrder);
+ 
+    console.log(userID);
+    if (userIDOrder != userID._id) {
+        res.send("user Id is not valid,  No such User in Our List !")
+    }
+    else if (productIDOrder != productId._id) {
+        res.send("product Id is not valid,  No such Product in Our List !")
+    }
+    else{
+        next();
+    }
+}
+module.exports.isFree = isFree;
+module.exports.isOrderIDvalid = isOrderIDvalid;
